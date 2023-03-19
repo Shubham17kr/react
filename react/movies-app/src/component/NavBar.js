1ddaa000c14b369d778b47817546aac4fd5fe034
Shubham17kr <shubhamkumar97423@gmail.com>
2023-03-19 13:35:39 +0530

    banner created

--- react/movies-app/src/component/NavBar.js
+++ react/movies-app/src/component/NavBar.js
@@ -4,9 +4,9 @@ import { Component } from "react";
 class NavBar extends Component{
     render(){
         return(
-            <div style={{display:"flex"}}>
+            <div style={{display:"flex",color:"blue"}}>
                 <h1>Movies App</h1>
-                <h2 style={{marginLeft:"2rem",marginTop:"2rem"}}>Favourites</h2>
+                <h2 style={{marginLeft:"2rem",marginTop:"0.7rem"}}>Favourites</h2>
             </div>
         )
     }
