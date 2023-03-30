f4ac95d235896df73a6195afc32c5895b2e8706f
Shubham17kr <shubhamkumar97423@gmail.com>
2023-03-30 14:51:54 +0530

    search bar created

--- react/movies-app/src/component/Fav.js
+++ react/movies-app/src/component/Fav.js
@@ -9,7 +9,8 @@ class Fav extends Component {
     this.state={
       genres:[],
       currGenre:"all genres",
-      movie:[]
+      movie:[],
+      currText:""
     }
   }
 
@@ -61,9 +62,39 @@ class Fav extends Component {
 
      })
   }
-  
 
   }
+
+  handleCurrText=(inputValue)=>{
+
+    this.setState({
+      currText:inputValue
+    },this.searchMovies)
+  }
+
+  searchMovies=()=>{
+
+    if(this.state.currText != "")
+    {
+      let filteredArr= this.state.movie.filter((movieObj)=>{
+
+        let title=movieObj.original_title.toLowerCase();
+        return title.includes(this.state.currText.toLowerCase())
+      })
+
+      this.setState({
+        movie:[...filteredArr]
+      })
+    }
+    else{
+      let data=JSON.parse(localStorage.getItem('movies-app') || '[]')
+      this.setState({
+        movie:[...data]
+      })
+    }
+  }
+
+
   render() {
     // let movieArr = movies.results;
     let genreId={28:"Action",12:"Adventure",16:"Animation",35:"Comedy",80:"Crime",99:"Documentary",18:"Drama",10751:"Family",14:"Fantasy",36:"History",27:"Horror",10402:"Music",9648:"Mystery",10749:"Romance",878:"Science Fiction",10770:"TV Movie",53:"Thriller",10752:"War",37:"Western"}
@@ -93,11 +124,7 @@ class Fav extends Component {
 
           <div className="col-9 fav-table">
             <div className="row">
-              <input
-                type="text"
-                class="form-control col"
-                placeholder="Search"
-              />
+              <input type="text" class="form-control col" placeholder="Search" value={this.state.currText} onChange={(e)=>{this.handleCurrText(e.target.value)}}/>
               <input type="text" class="form-control col" />
             </div>
 
